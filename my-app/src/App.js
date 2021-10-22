9cc3af2a9b0586b098d4cf089f51b1cf8152c00a
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-10-22 15:37:08 +0300

    Feat: start weather API app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// const InputDays = () => {
+//   return (
+//     <input type="text" name = 'days' placeholder="Введите количество дней" className ="days" onChange = {handleChange}/>
+//   )
+// }
+
+// const InputCity = () => {
+//   return (
+//     <input type="text" name ='city' placeholder="Введите город" className ="city"  />
+//   )
+// }
+
+// const InputBtn = () => {
+//   return (
+//     <input type="button" value ='run' className ='btn' />
+//   )
+// }
+
+// const Inputted = () => {
+//   return (
+//     <div className = 'inputted'>
+//             <form name = "inputs">
+//             <InputDays/>
+//             <InputCity/>
+//             <InputBtn/>
+//             </form>
+//     </div>
+//   )
+// }
+
+// const Outputted = () => {
+//   <div class = 'outputted'> </div>
+// }
+
+const Outputted = () => {
+  return ( <div class = 'outputted'> </div>)}
+
+const App = () => {
+
+  function handleChangeDays (event) {
+    localStorage.setItem("inputDays", event.target.value)
+  }
+
+  function handleChangeCity (event) {
+    localStorage.setItem("inputCity", event.target.value)
+  }
+
+  function clickRun () {
+    const days = localStorage.getItem('inputDays');
+    const city = localStorage.getItem('inputCity');
+
+
+//здесь должно быть создание самого запроса
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container'>
+      <div className = 'inputted'>
+            <form name = "inputs">
+            <input type="text" name = 'days' placeholder="Введите количество дней" className ="days" onChange = {handleChangeDays}/>
+      <input type="text" name ='city' placeholder="Введите город" className ="city" onChange = {handleChangeCity} />
+      <input type="button" value ='run' className ='btn' onClick = {clickRun}/>
+            </form>
     </div>
-  );
+    <Outputted/>
+</div>
+  )
 }
 
 export default App;
