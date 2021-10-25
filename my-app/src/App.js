be02da8d39b2e2d976735570dd5228ca31af9a89
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-10-25 12:48:55 +0300

    feat: add API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,74 +1,71 @@
-import React from 'react';
+import React, { JSX, useState } from "react";
 import './App.css';
 
-// const InputDays = () => {
-//   return (
-//     <input type="text" name = 'days' placeholder="Введите количество дней" className ="days" onChange = {handleChange}/>
-//   )
-// }
-
-// const InputCity = () => {
-//   return (
-//     <input type="text" name ='city' placeholder="Введите город" className ="city"  />
-//   )
-// }
-
-// const InputBtn = () => {
-//   return (
-//     <input type="button" value ='run' className ='btn' />
-//   )
-// }
-
-// const Inputted = () => {
-//   return (
-//     <div className = 'inputted'>
-//             <form name = "inputs">
-//             <InputDays/>
-//             <InputCity/>
-//             <InputBtn/>
-//             </form>
-//     </div>
-//   )
-// }
-
-// const Outputted = () => {
-//   <div class = 'outputted'> </div>
-// }
-
-const Outputted = () => {
-  return ( <div class = 'outputted'> </div>)}
-
 const App = () => {
+  const [state, setState] = useState({
+    value: "",
+    days: "",
+    city: ""
+  });
 
-  function handleChangeDays (event) {
-    localStorage.setItem("inputDays", event.target.value)
-  }
+  const handleChangeEvent = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
 
-  function handleChangeCity (event) {
-    localStorage.setItem("inputCity", event.target.value)
-  }
-
-  function clickRun () {
-    const days = localStorage.getItem('inputDays');
-    const city = localStorage.getItem('inputCity');
-
-
-//здесь должно быть создание самого запроса
-
-  }
+  const clickRun = () => {
+    const { days, city } = state;
+    console.log(days, city);
+    const url = "https://api.m3o.com/v1/weather/Forecast";
+    fetch(url + `?days=${days}&location=${city}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer NWZlYzNjNTEtZjYyOC00YzZjLTk4MzYtYTk3ZTlkNWVkMDA1"
+      }
+    })
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log("DATA: ", data);
+        setState({
+          ...state,
+          value: data
+        });
+      })
+      .catch(function (error) {
+        setState({
+          ...state,
+          value: error
+        });
+      });
+  };
 
   return (
-    <div className = 'container'>
-      <div className = 'inputted'>
-            <form name = "inputs">
-            <input type="text" name = 'days' placeholder="Введите количество дней" className ="days" onChange = {handleChangeDays}/>
-      <input type="text" name ='city' placeholder="Введите город" className ="city" onChange = {handleChangeCity} />
-      <input type="button" value ='run' className ='btn' onClick = {clickRun}/>
-            </form>
+    <div className="container">
+      <div className="inputted">
+        <form name="inputs">
+          <input
+            type="text"
+            name="days"
+            placeholder="Введите количество дней"
+            className="days"
+            onChange={handleChangeEvent}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="Введите город"
+            className="city"
+            onChange={handleChangeEvent}
+          />
+          <input type="button" value="run" className="btn" onClick={clickRun} />
+        </form>
+      </div>
+      <div className="outputted">{JSON.stringify(state.value, null, '\t')} </div>
     </div>
-    <Outputted/>
-</div>
-  )
-}
+  );
+};
 
 export default App;
